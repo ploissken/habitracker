180af87d84adb1b95a2d37d895771bb3fa64a11d
Rodrigo Souza <rodrigo.souza@producepay.com>
2024-04-26 11:13:58 -0300

    first commit, express, cors, body-parser and dotenv

---
+++ src/server.js
@@ -0,0 +1,25 @@
+const PORT = process.env.PORT || 9000;
+
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+app.use(
+  bodyParser.urlencoded({ extended: false }),
+  bodyParser.json(),
+  cors({
+    credentials: true,
+    origin: [
+      "http://habitica.com",
+      "https://habitica.com",
+      "http://localhost:3000",
+    ],
+  })
+);
+
+app.listen(PORT, () => {
+  console.info(`\x1b[1m\x1b[32m[server] listening on port ${PORT}\x1b[0m`);
+});
+
+module.exports = app;
